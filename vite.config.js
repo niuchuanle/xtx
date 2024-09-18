9dcd7ad3ff57a545f8cf68f981ca8b1ef651eb50
“niuchuanle” <2110908472@qq.com>
2024-09-18 08:59:03 +0800

    elementui-plus

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 // elementpluys 按需导入
 import AutoImport from 'unplugin-auto-import/vite'
@@ -8,18 +8,29 @@ import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-      AutoImport({
-        resolvers:[ElementPlusResolver()],
-      }),
-      Components({
-        resolvers:[ElementPlusResolver()],
-      }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [
+                ElementPlusResolver({importStyle: "sass"})
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css:{
+        preprocessorOptions:{
+            scss:{
+                additionalData:`
+                @use "@/styles/element/index.scss" as *;
+                `,
+            }
+        }
     }
-  }
 })
